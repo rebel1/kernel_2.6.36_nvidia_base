144065eea12d4750b83de547fabbe25ad8bc7dfb
Chao Jiang <chaoj@nvidia.com>
2010-11-30 20:50:33 +0900

    video: tegra: support output pin polarities setting
    Add support for set up pin polarities in DC driver.
    
    fixes bug 758356
    
    Change-Id: Iff7026929d73e9cf232baeda1f9f0c8bbd60b66b
    Reviewed-on: http://git-master/r/11674
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/include/mach/dc.h
+++ arch/arm/mach-tegra/include/mach/dc.h
@@ -44,6 +44,23 @@ enum {
 	TEGRA_DC_OUT_HDMI,
 };
 
+struct tegra_dc_out_pin {
+	int     name;
+	int     pol;
+};
+
+enum {
+	TEGRA_DC_OUT_PIN_DATA_ENABLE,
+	TEGRA_DC_OUT_PIN_H_SYNC,
+	TEGRA_DC_OUT_PIN_V_SYNC,
+	TEGRA_DC_OUT_PIN_PIXEL_CLOCK,
+};
+
+enum {
+	TEGRA_DC_OUT_PIN_POL_LOW,
+	TEGRA_DC_OUT_PIN_POL_HIGH,
+};
+
 struct tegra_dc_out {
 	int			type;
 	unsigned		flags;
@@ -61,6 +78,9 @@ struct tegra_dc_out {
 
 	struct tegra_dc_mode	*modes;
 	int			n_modes;
+
+	struct tegra_dc_out_pin	*out_pins;
+	unsigned		n_out_pins;
 
 	int	(*enable)(void);
 	int	(*disable)(void);
