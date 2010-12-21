fe849ad8b1c6657b0b7820e82ef4d9cbb4352d57
Jon Mayo <jmayo@nvidia.com>
2010-12-21 15:09:33 -0800

    [ARM] tegra: use APB DMA for accessing APB devices
    Change-Id: I165411a14342666cbac02fb8cb171580ab0826aa
    Reviewed-on: http://git-master/r/14464
    Reviewed-by: Jonathan Mayo <jmayo@nvidia.com>
    Tested-by: Jonathan Mayo <jmayo@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- arch/arm/mach-tegra/apbio.h
+++ arch/arm/mach-tegra/apbio.h
@@ -1,10 +1,8 @@
 /*
- * arch/arm/mach-tegra/fuse.c
+ * arch/arm/mach-tegra/apbio.h
  *
+ * Copyright (C) 2010 NVIDIA Corporation.
  * Copyright (C) 2010 Google, Inc.
- *
- * Author:
- *	Colin Cross <ccross@android.com>
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -17,13 +15,6 @@
  *
  */
 
-unsigned long long tegra_chip_uid(void);
-unsigned int tegra_spare_fuse(int bit);
-int tegra_sku_id(void);
-int tegra_cpu_process_id(void);
-int tegra_core_process_id(void);
-int tegra_soc_speedo_id(void);
-void tegra_init_fuse(void);
-void tegra_init_speedo_data(void);
-u32 tegra_fuse_readl(unsigned long offset);
-void tegra_fuse_writel(u32 value, unsigned long offset);
+u32 tegra_apb_readl(unsigned long offset);
+void tegra_apb_writel(u32 value, unsigned long offset);
+void tegra_init_apb_dma(void);
