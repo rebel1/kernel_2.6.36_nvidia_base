80f89d2f713c0b51a74e992b6f08beb3dc73eed7
Pierre-Loup A. Griffais <pgriffais@nvidia.com>
2010-12-27 17:52:09 -0800

    tegra: harmony: pull recent board support from the chrome tree
    Also adds a known-good default config.
    
    Change-Id: I5e86917571a64174c10f5cc21ea83748cc22bbf5
    Reviewed-on: http://git-master/r/14423
    Reviewed-by: Pierre-Loup Griffais <pgriffais@nvidia.com>
    Tested-by: Pierre-Loup Griffais <pgriffais@nvidia.com>
    Reviewed-by: Daniel Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/board-harmony.c
+++ arch/arm/mach-tegra/board-harmony.c
@@ -23,23 +23,29 @@
 #include <linux/mtd/partitions.h>
 #include <linux/dma-mapping.h>
 #include <linux/pda_power.h>
+#include <linux/i2c.h>
+#include <linux/i2c-tegra.h>
 #include <linux/io.h>
 #include <linux/delay.h>
+#include <linux/tegra_usb.h>
 
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/time.h>
 #include <asm/setup.h>
 
+#include <mach/audio.h>
+#include <mach/i2s.h>
 #include <mach/iomap.h>
 #include <mach/irqs.h>
 #include <mach/nand.h>
 #include <mach/clk.h>
+#include <mach/usb_phy.h>
+#include <mach/suspend.h>
 
 #include "clock.h"
 #include "board.h"
 #include "board-harmony.h"
-#include "clock.h"
 #include "devices.h"
 
 /* NVidia bootloader tags */
@@ -62,10 +68,25 @@ struct tag_tegra {
 
 static int __init parse_tag_nvidia(const struct tag *tag)
 {
-
 	return 0;
 }
 __tagtable(ATAG_NVIDIA, parse_tag_nvidia);
+
+static struct tegra_utmip_config utmi_phy_config = {
+	.hssync_start_delay = 0,
+	.idle_wait_delay = 17,
+	.elastic_limit = 16,
+	.term_range_adj = 6,
+	.xcvr_setup = 9,
+	.xcvr_lsfslew = 2,
+	.xcvr_lsrslew = 2,
+};
+
+static struct tegra_ehci_platform_data tegra_ehci_pdata = {
+	.phy_config = &utmi_phy_config,
+	.operating_mode = TEGRA_USB_HOST,
+	.power_down_on_bus_suspend = 1,
+};
 
 static struct tegra_nand_chip_parms nand_chip_parms[] = {
 	/* Samsung K5E2G1GACM */
@@ -164,21 +185,90 @@ static struct platform_device pda_power_device = {
 	},
 };
 
+static struct tegra_i2c_platform_data harmony_i2c1_platform_data = {
+	.adapter_nr	= 0,
+	.bus_count	= 1,
+	.bus_clk_rate	= { 400000, 0 },
+};
+
+static const struct tegra_pingroup_config i2c2_ddc = {
+	.pingroup	= TEGRA_PINGROUP_DDC,
+	.func		= TEGRA_MUX_I2C2,
+};
+
+static const struct tegra_pingroup_config i2c2_gen2 = {
+	.pingroup	= TEGRA_PINGROUP_PTA,
+	.func		= TEGRA_MUX_I2C2,
+};
+
+static struct tegra_i2c_platform_data harmony_i2c2_platform_data = {
+	.adapter_nr	= 1,
+	.bus_count	= 2,
+	.bus_clk_rate	= { 400000, 100000 },
+	.bus_mux	= { &i2c2_ddc, &i2c2_gen2 },
+	.bus_mux_len	= { 1, 1 },
+};
+
+static struct tegra_i2c_platform_data harmony_i2c3_platform_data = {
+	.adapter_nr	= 3,
+	.bus_count	= 1,
+	.bus_clk_rate	= { 400000, 0 },
+};
+
+static struct tegra_i2c_platform_data harmony_dvc_platform_data = {
+	.adapter_nr	= 4,
+	.bus_count	= 1,
+	.bus_clk_rate	= { 400000, 0 },
+	.is_dvc		= true,
+};
+
+static struct i2c_board_info __initdata harmony_i2c_bus1_board_info[] = {
+	{
+		I2C_BOARD_INFO("wm8903", 0x1a),
+	},
+};
+
+static struct tegra_audio_platform_data tegra_audio_pdata = {
+	.i2s_master	= false,
+	.dsp_master	= false,
+	.dma_on		= true,  /* use dma by default */
+	.i2s_clk_rate	= 240000000,
+	.dap_clk	= "clk_dev1",
+	.audio_sync_clk = "audio_2x",
+	.mode		= I2S_BIT_FORMAT_I2S,
+	.fifo_fmt	= I2S_FIFO_16_LSB,
+	.bit_size	= I2S_BIT_SIZE_16,
+};
+
+static void harmony_i2c_init(void)
+{
+	tegra_i2c_device1.dev.platform_data = &harmony_i2c1_platform_data;
+	tegra_i2c_device2.dev.platform_data = &harmony_i2c2_platform_data;
+	tegra_i2c_device3.dev.platform_data = &harmony_i2c3_platform_data;
+	tegra_i2c_device4.dev.platform_data = &harmony_dvc_platform_data;
+
+	platform_device_register(&tegra_i2c_device1);
+	platform_device_register(&tegra_i2c_device2);
+	platform_device_register(&tegra_i2c_device3);
+	platform_device_register(&tegra_i2c_device4);
+
+	i2c_register_board_info(0, harmony_i2c_bus1_board_info,
+				ARRAY_SIZE(harmony_i2c_bus1_board_info));
+}
+
 static struct platform_device *harmony_devices[] __initdata = {
 	&debug_uart,
 	&pmu_device,
 	&tegra_nand_device,
 	&tegra_udc_device,
 	&pda_power_device,
-	&tegra_i2c_device1,
-	&tegra_i2c_device2,
-	&tegra_i2c_device3,
-	&tegra_i2c_device4,
+	&tegra_ehci3_device,
 	&tegra_spi_device1,
 	&tegra_spi_device2,
 	&tegra_spi_device3,
 	&tegra_spi_device4,
 	&tegra_gart_device,
+	&tegra_i2s_device1,
 };
 
 static void __init tegra_harmony_fixup(struct machine_desc *desc,
@@ -193,22 +283,124 @@ static void __init tegra_harmony_fixup(struct machine_desc *desc,
 
 static __initdata struct tegra_clk_init_table harmony_clk_init_table[] = {
 	/* name		parent		rate		enabled */
-	{ "uartd",	"pll_p",	216000000,	true },
+	{ "clk_dev1",	NULL,		26000000,	true},
+	{ "clk_m",	NULL,		12000000,	true},
+	{ "3d",		"pll_m",	266400000,	true},
+	{ "2d",		"pll_m",	266400000,	true},
+	{ "vi",		"pll_m",	50000000,	true},
+	{ "vi_sensor",	"pll_m",	111000000,	false},
+	{ "epp",	"pll_m",	266400000,	true},
+	{ "mpe",	"pll_m",	111000000,	false},
+	{ "emc",	"pll_m",	666000000,	true},
+	{ "pll_c",	"clk_m",	600000000,	true},
+	{ "pll_c_out1",	"pll_c",	240000000,	true},
+	{ "vde",	"pll_c",	240000000,	false},
+	{ "pll_p",	"clk_m",	216000000,	true},
+	{ "pll_p_out1",	"pll_p",	28800000,	true},
+	{ "pll_a",	"pll_p_out1",	56448000,	true},
+	{ "pll_a_out0",	"pll_a",	11289600,	true},
+	{ "i2s1",	"pll_a_out0",	11289600,	true},
+	{ "audio",	"pll_a_out0",	11289600,	true},
+	{ "audio_2x",	"audio",	22579200,	false},
+	{ "pll_p_out2",	"pll_p",	48000000,	true},
+	{ "pll_p_out3",	"pll_p",	72000000,	true},
+	{ "i2c1_i2c",	"pll_p_out3",	72000000,	true},
+	{ "i2c2_i2c",	"pll_p_out3",	72000000,	true},
+	{ "i2c3_i2c",	"pll_p_out3",	72000000,	true},
+	{ "dvc_i2c",	"pll_p_out3",	72000000,	true},
+	{ "csi",	"pll_p_out3",	72000000,	false},
+	{ "pll_p_out4",	"pll_p",	108000000,	true},
+	{ "sclk",	"pll_p_out4",	108000000,	true},
+	{ "hclk",	"sclk",		108000000,	true},
+	{ "pclk",	"hclk",		54000000,	true},
+	{ "apbdma",	"hclk",		54000000,	true},
+	{ "spdif_in",	"pll_p",	36000000,	false},
+	{ "csite",	"pll_p",	144000000,	true},
+	{ "uartd",	"pll_p",	216000000,	true},
+	{ "host1x",	"pll_p",	144000000,	true},
+	{ "disp1",	"pll_p",	216000000,	true},
+	{ "pll_d",	"clk_m",	1000000,	false},
+	{ "pll_d_out0",	"pll_d",	500000,		false},
+	{ "dsi",	"pll_d",	1000000,	false},
+	{ "pll_u",	"clk_m",	480000000,	true},
+	{ "clk_d",	"clk_m",	24000000,	true},
+	{ "timer",	"clk_m",	12000000,	true},
+	{ "i2s2",	"clk_m",	11289600,	false},
+	{ "spdif_out",	"clk_m",	12000000,	false},
+	{ "spi",	"clk_m",	12000000,	false},
+	{ "xio",	"clk_m",	12000000,	false},
+	{ "twc",	"clk_m",	12000000,	false},
+	{ "sbc1",	"clk_m",	12000000,	false},
+	{ "sbc2",	"clk_m",	12000000,	false},
+	{ "sbc3",	"clk_m",	12000000,	false},
+	{ "sbc4",	"clk_m",	12000000,	false},
+	{ "ide",	"clk_m",	12000000,	false},
+	{ "ndflash",	"clk_m",	108000000,	true},
+	{ "vfir",	"clk_m",	12000000,	false},
+	{ "sdmmc1",	"clk_m",	48000000,	true},
+	{ "sdmmc2",	"clk_m",	48000000,	true},
+	{ "sdmmc3",	"clk_m",	48000000,	false},
+	{ "sdmmc4",	"clk_m",	48000000,	true},
+	{ "la",		"clk_m",	12000000,	false},
+	{ "owr",	"clk_m",	12000000,	false},
+	{ "nor",	"clk_m",	12000000,	false},
+	{ "mipi",	"clk_m",	12000000,	false},
+	{ "i2c1",	"clk_m",	3000000,	false},
+	{ "i2c2",	"clk_m",	3000000,	false},
+	{ "i2c3",	"clk_m",	3000000,	false},
+	{ "dvc",	"clk_m",	3000000,	false},
+	{ "uarta",	"clk_m",	12000000,	false},
+	{ "uartb",	"clk_m",	12000000,	false},
+	{ "uartc",	"clk_m",	12000000,	false},
+	{ "uarte",	"clk_m",	12000000,	false},
+	{ "cve",	"clk_m",	12000000,	false},
+	{ "tvo",	"clk_m",	12000000,	false},
+	{ "hdmi",	"clk_m",	12000000,	false},
+	{ "tvdac",	"clk_m",	12000000,	false},
+	{ "disp2",	"clk_m",	12000000,	false},
+	{ "usbd",	"clk_m",	12000000,	false},
+	{ "usb2",	"clk_m",	12000000,	false},
+	{ "usb3",	"clk_m",	12000000,	true},
+	{ "isp",	"clk_m",	12000000,	false},
+	{ "csus",	"clk_m",	12000000,	false},
+	{ "pwm",	"clk_32k",	32768,		false},
+	{ "clk_32k",	NULL,		32768,		true},
+	{ "pll_s",	"clk_32k",	32768,		false},
+	{ "rtc",	"clk_32k",	32768,		true},
+	{ "kbc",	"clk_32k",	32768,		true},
 	{ NULL,		NULL,		0,		0},
+};
+
+static struct tegra_suspend_platform_data harmony_suspend = {
+	.cpu_timer = 5000,
+	.cpu_off_timer = 5000,
+	.core_timer = 0x7e7e,
+	.core_off_timer = 0x7f,
+	.separate_req = true,
+	.corereq_high = false,
+	.sysclkreq_high = true,
+	.suspend_mode = TEGRA_SUSPEND_LP0,
 };
 
 static void __init tegra_harmony_init(void)
 {
 	tegra_common_init();
 
+	tegra_init_suspend(&harmony_suspend);
+
 	tegra_clk_init_from_table(harmony_clk_init_table);
 
 	harmony_pinmux_init();
+
+	tegra_ehci3_device.dev.platform_data = &tegra_ehci_pdata;
+
+	tegra_i2s_device1.dev.platform_data = &tegra_audio_pdata;
 
 	platform_add_devices(harmony_devices, ARRAY_SIZE(harmony_devices));
 
 	harmony_panel_init();
 	harmony_sdhci_init();
+	harmony_i2c_init();
 }
 
 MACHINE_START(HARMONY, "harmony")
