cd1b3ab823dcda26f92e351b5170714e6ca31d9a
Ravindra Lokhande <rlokhande@nvidia.com>
2011-01-19 19:41:22 +0530

    [ARM] tegra: ventana: added members  to i2s data
    Added more members to i2s platform data. These members will be used
    by i2s driver to set i2s controller.
    
    Change-Id: I3bc6eb9845cb1275979e21f6b39dd4d5e18b832b
    Reviewed-on: http://git-master/r/16251
    Reviewed-by: Ravindra Lokhande <rlokhande@nvidia.com>
    Tested-by: Ravindra Lokhande <rlokhande@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Scott Peterson <speterson@nvidia.com>

--- arch/arm/mach-tegra/board-ventana.c
+++ arch/arm/mach-tegra/board-ventana.c
@@ -279,23 +279,35 @@ static struct tegra_i2c_platform_data ventana_dvc_platform_data = {
 static struct tegra_audio_platform_data tegra_audio_pdata[] = {
 	/* For I2S1 */
 	[0] = {
+		.i2s_master	= true,
+		.dsp_master	= false,
 		.dma_on		= true,  /* use dma by default */
+		.i2s_master_clk = 44100,
+		.dsp_master_clk = 8000,
 		.i2s_clk_rate	= 240000000,
 		.dap_clk	= "clk_dev1",
 		.audio_sync_clk = "audio_2x",
 		.mode		= I2S_BIT_FORMAT_I2S,
-		.fifo_fmt	= I2S_FIFO_16_LSB,
+		.fifo_fmt	= I2S_FIFO_PACKED,
 		.bit_size	= I2S_BIT_SIZE_16,
+		.i2s_bus_width = 32,
+		.dsp_bus_width = 16,
 	},
 	/* For I2S2 */
 	[1] = {
+		.i2s_master	= false,
+		.dsp_master	= true,
 		.dma_on		= true,  /* use dma by default */
+		.i2s_master_clk = 44100,
+		.dsp_master_clk = 8000,
 		.i2s_clk_rate	= 240000000,
 		.dap_clk	= "clk_dev1",
 		.audio_sync_clk = "audio_2x",
-		.mode		= I2S_BIT_FORMAT_I2S,
+		.mode		= I2S_BIT_FORMAT_DSP,
 		.fifo_fmt	= I2S_FIFO_16_LSB,
 		.bit_size	= I2S_BIT_SIZE_16,
+		.i2s_bus_width = 32,
+		.dsp_bus_width = 16,
 	}
 };
 
