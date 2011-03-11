8915048e4bd75b725de9486764e3b63c0e45eb80
Prashant Gaikwad <pgaikwad@nvidia.com>
2011-03-11 11:39:30 +0530

    [ARM] tegra: Enable EMC scaling
    Frequency tables added for memory.
    Enabled memory tables used for EMC scaling.
    
    Bug 786376
    
    Change-Id: I8f9713dac7950db4a42dac4f32d8908434c18be1
    Reviewed-on: http://git-master/r/22578
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- arch/arm/mach-tegra/board-whistler.c
+++ arch/arm/mach-tegra/board-whistler.c
@@ -565,6 +565,7 @@ static void __init tegra_whistler_init(void)
 	whistler_scroll_init();
 	whistler_codec_init();
 	whistler_power_off_init();
+	whistler_emc_init();
 }
 
 int __init tegra_whistler_protected_aperture_init(void)
