8c0f23b7e65ebdb6d1c160517e9ba509b16ec83f
Ken Chang <kenc@nvidia.com>
2011-03-16 12:55:15 +0800

    mmc: host: support runtime enable/disable SDCLK
    Implement functions needed in struct mmc_host_ops to support
    enable/disable SDCLK dynamically.
    Add SDHCI_QUIRK_RUNTIME_DISABLE to switch on/off this feature.
    
    Bug 800655
    
    Change-Id: I4c71ca4beb56df020381236ca8ec949647a0d333
    Reviewed-on: http://git-master/r/23125
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -150,7 +150,8 @@ static int __devinit tegra_sdhci_probe(struct platform_device *pdev)
 			SDHCI_QUIRK_NO_HISPD_BIT |
 			SDHCI_QUIRK_8_BIT_DATA |
 			SDHCI_QUIRK_NO_VERSION_REG |
-			SDHCI_QUIRK_BROKEN_ADMA_ZEROLEN_DESC;
+			SDHCI_QUIRK_BROKEN_ADMA_ZEROLEN_DESC |
+			SDHCI_QUIRK_RUNTIME_DISABLE;
 
 	if (plat->force_hs != 0)
 		sdhci->quirks |= SDHCI_QUIRK_FORCE_HIGH_SPEED_MODE;
