6610bc77e97d29d84c1cf55d4090ed57bb320b25
Prashant Gaikwad <pgaikwad@nvidia.com>
2011-04-28 15:48:44 +0530

    ARM: tegra: clocks: make pclk div dynamic
    dynamic changing of pclk divider to follow APB clock minimum
    frequency requirements with respect to sclk frequency.
    
    Bug 819796
    
    Reviewed-on: http://git-master/r/29643
    (cherry picked from commit c5ed952608ff2e3ffdcba99295f8892dac1506c0)
    
    Change-Id: Iec403b137fa001ff401fd14990040889ec679eca
    Reviewed-on: http://git-master/r/30315
    Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>
    Tested-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/clock.h
+++ arch/arm/mach-tegra/clock.h
@@ -132,6 +132,13 @@ struct clk {
 			struct clk			*backup;
 		} cpu;
 		struct {
+			struct clk			*pclk;
+			struct clk			*hclk;
+			struct clk			*sclk_low;
+			struct clk			*sclk_high;
+			unsigned long			threshold;
+		} system;
+		struct {
 			struct list_head		node;
 			bool				enabled;
 			unsigned long			rate;
